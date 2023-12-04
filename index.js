1989b18ea19cd6637bbd19e47505ad75ad7e7bf4
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2023-12-04 23:27:52 +0530

    Added : Delete method

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const {v4 : uuidv4} = require('uuid');
+const methodOverride= require('method-override');
+
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -52,6 +55,28 @@ app.post("/posts",(req,res)=>{
     let {username, content}= req.body;
     let id= uuidv4();
     posts.push({id, username, content});
+    res.redirect("/posts");
+})
+
+//Post Update
+app.patch("/posts/:id",(req,res)=>{
+    let { id }= req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id ===p.id);
+        post.content=newContent;
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id}=req.params;
+    let post= posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post});
+})
+
+//Post Delete
+app.delete("/posts/:id",(req,res)=>{
+    let {id}=req.params;
+    posts= posts.filter((p)=>id!==p.id);
     res.redirect("/posts");
 })
 
