c686fa10d8f12dfc72ccafd5c4cfef99d10ca200
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2023-12-04 22:09:06 +0530

    Added: Creating uuid, see post in detail

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const {v4 : uuidv4} = require('uuid');
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"views")));
+app.use(express.static(path.join(__dirname,"public")));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -16,23 +17,44 @@ app.get("/",(req,res)=>{
 
 let posts=[
     {
+        id : uuidv4(),
         username : "kaleem",
         content  : " I love coding!",
     },
     {
+        id : uuidv4(),
         username : "saqlain",
         content  : " I love music!",
     },
     {
+        id : uuidv4(),
         username : "ansari",
         content : "Bisalpur is best city",
     },
 ];
 
-app.get("/posts" (req,res)=>{
-    
+app.get("/posts", (req,res)=>{
+    res.render("index.ejs",{posts});
+})
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+app.get("/posts/:id",(req,res)=>{
+     let {id}=req.params;
+     let post= posts.find((p)=>id===p.id);
+
+     res.render("show.ejs",{post});
+})
+
+app.post("/posts",(req,res)=>{
+    let {username, content}= req.body;
+    let id= uuidv4();
+    posts.push({id, username, content});
+    res.redirect("/posts");
 })
 
 app.listen(port,()=>{
-    console.log("Server is running at port 8080");
+    console.log(`Server is running at port ${port}`);
 });
